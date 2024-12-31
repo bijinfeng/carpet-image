041bbecc6770d9f0143d6ca0ac5b91c3cca481a6
jfbi <jfbi@iflytek.com>
2024-12-31 16:47:40 +0800

    chore: delete github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import process from 'node:process'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default ({ mode }: { mode: string }) => {
-  const ENV = { ...process.env, ...loadEnv(mode, process.cwd()) }
-  const isDev = ENV.NODE_ENV === 'development'
-
+export default () => {
   return defineConfig({
-    base: isDev ? '/' : '/carpet-image/',
     plugins: [vue()],
     resolve: {
       alias: {
