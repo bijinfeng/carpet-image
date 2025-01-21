9270ecaa683cfcf698b97ae6ce7b742129c44b39
jfbi <jfbi@iflytek.com>
2025-01-21 23:33:49 +0800

    feat: add aptabase

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { init } from '@aptabase/web'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import VueKonva from 'vue-konva'
 
+import { version } from '../package.json'
 import App from './App.vue'
 import './assets/index.css'
+
+init('A-SH-2497555130', {
+  host: 'https://aptabase.fengrui.xyz',
+  appVersion: version,
+  isDebug: import.meta.env.DEV,
+})
 
 const pinia = createPinia()
 const app = createApp(App)
