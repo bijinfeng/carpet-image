b2df1021f427063bac2ff353e40054d9c7262ec6
jfbi <jfbi@iflytek.com>
2025-01-21 14:48:41 +0800

    feat: add stage force render key

--- src/components/canvas-container/index.vue
+++ src/components/canvas-container/index.vue
@@ -5,16 +5,22 @@ import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 import { useLayoutStore } from '@/stores/layout'
 import { Download } from 'lucide-vue-next'
+import { storeToRefs } from 'pinia'
 
-import { useTemplateRef } from 'vue'
+import { computed, useTemplateRef } from 'vue'
 import Container from './container.vue'
 import FormControl from './form.vue'
 import Render from './render.vue'
 import ScaleControl from './scale-control.vue'
 
 const layoutStore = useLayoutStore()
+const { contextState, activeCarpet } = storeToRefs(layoutStore)
 
 const canvasRef = useTemplateRef<{ exportToImage: () => void }>('canvasRef')
+
+const rendeKey = computed(() => {
+  return [contextState.value.width, contextState.value.height].concat(Object.values(contextState.value.radius)).join('_')
+})
 
 function handleDownload() {
   canvasRef.value?.exportToImage()
@@ -24,7 +30,7 @@ function handleDownload() {
 <template>
   <div class="flex h-full flex-col">
     <div class="relative flex items-center px-4 py-2 h-[52px]">
-      <Label class="leading-9 font-bold">{{ layoutStore.contextState.carpetName }}</Label>
+      <Label class="leading-9 font-bold">{{ contextState.carpetName }}</Label>
       <div class="absolute flex items-center right-4 gap-2">
         <ScaleControl />
         <Tooltip content="下载">
@@ -38,8 +44,8 @@ function handleDownload() {
     <Separator />
     <div class="flex flex-1">
       <Container>
-        <Render ref="canvasRef">
-          <component :is="layoutStore.activeCarpet.renderCanvas(layoutStore.contextState)" />
+        <Render ref="canvasRef" :key="rendeKey">
+          <component :is="activeCarpet.renderCanvas(contextState)" />
         </Render>
       </Container>
       <Separator orientation="vertical" />
