e373d401e7052192a1ed88295875bc6ef253bf55
jfbi <jfbi@iflytek.com>
2025-02-20 11:46:36 +0800

    chore: 将 aptabase 注册成一个 vue 插件

--- src/helper/aptabase.ts
+++ src/helper/aptabase.ts
@@ -1,16 +1,26 @@
-import { init, trackEvent } from '@aptabase/web'
-import { version } from '../../package.json'
+import { init, trackEvent } from '@aptabase/web';
+import type { App } from 'vue';
+import { version } from '../../package.json';
 
 init('A-SH-2497555130', {
-  host: 'https://aptabase.fengrui.xyz',
-  appVersion: version,
-  isDebug: import.meta.env.DEV,
-})
+	host: 'https://aptabase.fengrui.xyz',
+	appVersion: version,
+	isDebug: import.meta.env.DEV,
+});
 
 export function appMountedEvent() {
-  trackEvent('app_mounted')
+	trackEvent('app_mounted');
 }
 
 export function downloadEvent() {
-  trackEvent('download')
+	trackEvent('download');
 }
+
+export const install = (app: App) => {
+	app.config.globalProperties.$aptabase = {
+		appMountedEvent,
+		downloadEvent,
+	};
+
+	appMountedEvent();
+};
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import { appMountedEvent } from './helper/aptabase';
+import * as aptabase from './helper/aptabase';
 import './assets/index.css';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
-app.mount('#app').$nextTick(() => {
-	appMountedEvent();
-});
+app.use(aptabase);
+
+app.mount('#app');
